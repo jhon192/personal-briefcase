7be71a7faeb5009fb5de84df45f8774336c68e7e
jhon192 <jhon19.2@outlook.com>
2022-03-01 19:44:07 -0400

    components updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 // import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
+import Head from "next/head";
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <section className="max-w-screen max-h-full bg-blue-900">
+    <Head>
+      <link rel="shortcut icon" href="briefcase.svg" type="image/x-icon" />
+      <title>Personal Briefcase</title>
+    </Head>
       <Component {...pageProps} />
     </section>
   );
