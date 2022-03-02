4e7d4313c5c8db5fa49d91c2c680ffd0a64d2f4f
jhon192 <jhon19.2@outlook.com>
2022-03-01 20:23:07 -0400

    responsive design fixed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import { SiGithub, SiLinux } from "react-icons/si";
 const Navbar = () => {
   return (
     <div className="mb-2 max-w-full md:mx-10 m-0 bg-sky-100 sticky top-0 shadow-lg z-10 rounded-b-md">
-      <nav className="flex items-center justify-between px-10 py-2">
+      <nav className="flex items-center justify-between px-5 py-2">
         <div className="">
           <h1 className="md:text-3xl text-sm text-black hover:text-blue-800 transform hover:scale-125 transition-all">
             {" "}
@@ -12,16 +12,16 @@ const Navbar = () => {
         </div>
         <div className="w-2/3">
           <ul className="flex justify-around items-center">
-            <li className="md:text-xl text-sm text-black hover:text-blue-800 transform hover:scale-125 px-3 py-2 transition-all">
+            <li className="md:text-xl text-sm text-black hover:text-blue-800 transform hover:scale-125 p-1 transition-all">
               <a href="#beginning">Inicio</a>
             </li>
-            <li className="md:text-xl text-sm text-black hover:text-blue-800 transform hover:scale-125 px-3 py-2 transition-all">
+            <li className="md:text-xl text-sm text-black hover:text-blue-800 transform hover:scale-125 p-1 transition-all">
               <a href="#skills">Habilidades</a>
             </li>
-            <li className="md:text-xl text-sm text-black hover:text-blue-800 transform hover:scale-125 px-3 py-2 transition-all">
+            <li className="md:text-xl text-sm text-black hover:text-blue-800 transform hover:scale-125 p-1 transition-all">
               <a href="#projects">Proyectos</a>
             </li>
-            <li className="md:text-xl text-sm text-black hover:text-blue-800 transform hover:scale-125 px-3 py-2 transition-all">
+            <li className="md:text-xl text-sm text-black hover:text-blue-800 transform hover:scale-125 p-1 transition-all">
               <a href="#contact">Contactos</a>
             </li>
           </ul>
