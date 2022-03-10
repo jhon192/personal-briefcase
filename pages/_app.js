9658258cb01f4289d7574c70516ec2ebd8e29b12
jhon192 <jhon19.2@outlook.com>
2022-03-10 16:37:39 -0400

    update head tag

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,20 @@
 import "../styles/globals.css";
 import Head from "next/head";
 
-
 function MyApp({ Component, pageProps }) {
   return (
     <section className="max-w-screen max-h-full bg-blue-900">
-    <Head>
-      <link rel="shortcut icon" href="briefcase.svg" type="image/x-icon" />
-      <title>Personal Briefcase</title>
-      <meta name="description" content="Personal briefcase created using next.js, tailwind and other libraries and frameworks, created by Jhon Garcia" />
-    </Head>
+      <Head>
+        <link rel="shortcut icon" href="briefcase.svg" type="image/x-icon" />
+        <title>Personal Briefcase</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="author" content="Jhon Garcia"/>
+        <meta
+          name="description"
+          content="Personal briefcase created using next.js, tailwind and other libraries and frameworks, created by Jhon Garcia"
+        />
+      </Head>
       <Component {...pageProps} />
     </section>
   );
